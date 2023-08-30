75aca04934b351a24a37a3d1443a8ec36c4bbb29
ArnebNihal <104132526+ArnebNihal@users.noreply.github.com>
2023-08-30 14:52:13 +0200

    Add files via upload

--- Assets/Scripts/Game/MagicAndEffects/EntityEffectBroker.cs
+++ Assets/Scripts/Game/MagicAndEffects/EntityEffectBroker.cs
@@ -711,20 +711,6 @@ namespace DaggerfallWorkshop.Game.MagicAndEffects
             return false;
         }
 
-        /// <summary>
-        /// Lookup default spell name from standard spell dictionary.
-        /// This name is not localized. Result will be from SPELLS.STD or mods.
-        /// </summary>
-        /// <param name="id">ID of spell.</param>
-        /// <returns>Spell name if found, otherwise null.</returns>
-        public string GetStandardSpellName(int id)
-        {
-            if (standardSpells.ContainsKey(id))
-                return standardSpells[id].spellName;
-            else
-                return null;
-        }
-
         #endregion
 
         #region Private Methods
@@ -872,7 +858,7 @@ namespace DaggerfallWorkshop.Game.MagicAndEffects
                 BundleType = bundleType,
                 TargetType = ClassicTargetIndexToTargetType(spellRecordData.rangeType),
                 ElementType = ClassicElementIndexToElementType(spellRecordData.element),
-                Name = TextManager.Instance.GetLocalizedSpellName(spellRecordData.index),
+                Name = spellRecordData.spellName,
                 IconIndex = spellRecordData.icon,
                 Icon = new SpellIcon(),
                 StandardSpellIndex = spellRecordData.index, 
